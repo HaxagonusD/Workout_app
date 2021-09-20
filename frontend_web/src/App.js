86e1f2bcc2620986470708c52b04e61884a52031
HaxagonusD <jquezad1@binghamton.edu>
2021-09-19 20:58:56 -0400

    Added test document to the website

--- frontend_web/src/App.js
+++ frontend_web/src/App.js
@@ -1,5 +1,6 @@
 //import dependenies 
-import {app, auth} from "./firebaseSetUp"
+import {app, auth, db} from "./firebaseSetUp"
+import { collection, addDoc } from "firebase/firestore"; 
 //import hooks
 import {useAuthState} from "react-firebase-hooks/auth"
 
@@ -11,14 +12,31 @@ import Workout from './components/Workout';
 import SignIn from "./components/SignIn"
 
 
+
+
 function App() {
   //user object
   //null if user is not signed in populated if user is signed in
+
+  const addTestDocument = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
   const [user] = useAuthState(auth)
   return (
     <div>
      
      { user ? <Workout/>: <SignIn/>}
+     <button onClick={addTestDocument}>Add Test Document </button>
     </div>
     
   );
